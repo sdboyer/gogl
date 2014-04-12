9969def8364095daeb7cf9162f367b0192010a2f
Sam Boyer <tech@samboyer.org>
2014-04-12 12:56:51 -0400

    Move edge structures into their own file

---
+++ edge.go
@@ -0,0 +1,50 @@
+package gogl
+
+// A graph's behaviors are primarily a product of the constraints and
+// capabilities it places on its edges. These constraints and capabilities
+// determine whether certain types of operations are possible on the graph, as
+// well as the efficiencies for various operations.
+
+// gogl aims to provide a range of graph implementations that can meet
+// the varying constraints and implementation needs, but still achieve optimal
+// performance given those constraints.
+
+type Edge interface {
+	Source() Vertex
+	Target() Vertex
+	Both() (Vertex, Vertex)
+}
+
+type WeightedEdge interface {
+	Edge
+	Weight() int
+}
+
+// BaseEdge is a struct used internally to represent edges and meet the Edge
+// interface requirements. It uses the standard notation, (u,v), for vertex
+// pairs in an edge.
+type BaseEdge struct {
+	U Vertex
+	V Vertex
+}
+
+func (e BaseEdge) Source() Vertex {
+	return e.U
+}
+
+func (e BaseEdge) Target() Vertex {
+	return e.V
+}
+
+func (e BaseEdge) Both() (Vertex, Vertex) {
+	return e.U, e.V
+}
+
+type BaseWeightedEdge struct {
+	BaseEdge
+	W int
+}
+
+func (e BaseWeightedEdge) Weight() int {
+	return e.W
+}
--- graph.go
+++ graph.go
@@ -14,57 +14,6 @@ import (
 // does not.  Consequently, anything can act as a vertex.
 type Vertex interface{}
 
-/* Edge structures */
-
-// A graph's behaviors is primarily a product of the constraints and
-// capabilities it places on its edges. These constraints and capabilities
-// determine whether certain types of operations are possible on the graph, as
-// well as the efficiencies for various operations.
-
-// gogl aims to provide a diverse range of graph implementations that can meet
-// the varying constraints and implementation needs, but still achieve optimal
-// performance given those constraints.
-
-type Edge interface {
-	Source() Vertex
-	Target() Vertex
-	Both() (Vertex, Vertex)
-}
-
-type WeightedEdge interface {
-	Edge
-	Weight() int
-}
-
-// BaseEdge is a struct used internally to represent edges and meet the Edge
-// interface requirements. It uses the standard notation, (u,v), for vertex
-// pairs in an edge.
-type BaseEdge struct {
-	U Vertex
-	V Vertex
-}
-
-func (e BaseEdge) Source() Vertex {
-	return e.U
-}
-
-func (e BaseEdge) Target() Vertex {
-	return e.V
-}
-
-func (e BaseEdge) Both() (Vertex, Vertex) {
-	return e.U, e.V
-}
-
-type BaseWeightedEdge struct {
-	BaseEdge
-	W int
-}
-
-func (e BaseWeightedEdge) Weight() int {
-	return e.W
-}
-
 /* Graph structures */
 
 // Graph is gogl's most basic interface: it contains only the methods that
