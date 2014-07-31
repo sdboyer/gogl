efe6efeb41a1dff70d529e464c4d6cad447d0e33
Sam Boyer <tech@samboyer.org>
2014-07-31 00:52:03 -0400

    Introduce enum->slice/collection functors.

--- util.go
+++ util.go
@@ -84,3 +84,139 @@ func CopyGraph(from Graph, to interface{}) interface{} {
 
 	return g
 }
+
+/* Enumerator to slice/collection functors */
+
+// Internal interface used for granular checks on whether a graphish can report vertex count.
+type vertex_counter interface {
+	Order() int
+}
+
+// Internal interface used for granular checks on whether a graphish can report edge count.
+type edge_counter interface {
+	Size() int
+}
+
+// Collects all of a graph's vertices into a vertex slice, for easy range-ing.
+//
+// This is a convenience function. Avoid it on very large graphs or in performance critical sections.
+func CollectVertices(g VertexEnumerator) (vertices []Vertex) {
+	if c, ok := g.(vertex_counter); ok {
+		// If possible, size the slice based on the number of vertices the graph reports it has
+		vertices = make([]Vertex, 0, c.Order())
+	} else {
+		// Otherwise just pick something...reasonable?
+		vertices = make([]Vertex, 0, 32)
+	}
+
+	g.EachVertex(func(v Vertex) (terminate bool) {
+		vertices = append(vertices, v)
+		return
+	})
+
+	return vertices
+}
+
+// Collects all of a given vertex's adjacent vertices into a vertex slice, for easy range-ing.
+//
+// This is a convenience function. Avoid it on very large graphs or in performance critical sections.
+func CollectVerticesAdjacentTo(v Vertex, g AdjacencyEnumerator) (vertices []Vertex) {
+	if c, ok := g.(DegreeChecker); ok {
+		// If possible, size the slice based on the number of adjacent vertices the graph reports
+		deg, _ := c.DegreeOf(v)
+		vertices = make([]Vertex, 0, deg)
+	} else {
+		// Otherwise just pick something...reasonable?
+		vertices = make([]Vertex, 0, 8)
+	}
+
+	g.EachAdjacentTo(v, func(v Vertex) (terminate bool) {
+		vertices = append(vertices, v)
+		return
+	})
+
+	return vertices
+}
+
+// Collects all of a graph's edges into an edge slice, for easy range-ing.
+//
+// This is a convenience function. Avoid it on very large graphs or in performance critical sections.
+func CollectEdges(g EdgeEnumerator) (edges []Edge) {
+	if c, ok := g.(edge_counter); ok {
+		// If possible, size the slice based on the number of edges the graph reports it has
+		edges = make([]Edge, 0, c.Size())
+	} else {
+		// Otherwise just pick something...reasonable?
+		edges = make([]Edge, 0, 32)
+	}
+
+	g.EachEdge(func(e Edge) (terminate bool) {
+		edges = append(edges, e)
+		return
+	})
+
+	return edges
+}
+
+// Collects all of a given vertex's incident edges into an edge slice, for easy range-ing.
+//
+// This is a convenience function. Avoid it on very large graphs or in performance critical sections.
+func CollectEdgesIncidentTo(v Vertex, g IncidentEdgeEnumerator) (edges []Edge) {
+	if c, ok := g.(DegreeChecker); ok {
+		// If possible, size the slice based on the number of incident edges the graph reports
+		deg, _ := c.DegreeOf(v)
+		edges = make([]Edge, 0, deg)
+	} else {
+		// Otherwise just pick something...reasonable?
+		edges = make([]Edge, 0, 8)
+	}
+
+	g.EachEdgeIncidentTo(v, func(e Edge) (terminate bool) {
+		edges = append(edges, e)
+		return
+	})
+
+	return edges
+}
+
+// Collects all of a given vertex's out-edges into an edge slice, for easy range-ing.
+//
+// This is a convenience function. Avoid it on very large graphs or in performance critical sections.
+func CollectArcsFrom(v Vertex, g IncidentArcEnumerator) (edges []Edge) {
+	if c, ok := g.(DirectedDegreeChecker); ok {
+		// If possible, size the slice based on the number of out-edges the graph reports
+		deg, _ := c.OutDegreeOf(v)
+		edges = make([]Edge, 0, deg)
+	} else {
+		// Otherwise just pick something...reasonable?
+		edges = make([]Edge, 0, 8)
+	}
+
+	g.EachArcFrom(v, func(e Edge) (terminate bool) {
+		edges = append(edges, e)
+		return
+	})
+
+	return edges
+}
+
+// Collects all of a given vertex's in-edges into an edge slice, for easy range-ing.
+//
+// This is a convenience function. Avoid it on very large graphs or in performance critical sections.
+func CollectArcsTo(v Vertex, g IncidentArcEnumerator) (edges []Edge) {
+	if c, ok := g.(DirectedDegreeChecker); ok {
+		// If possible, size the slice based on the number of in-edges the graph reports
+		deg, _ := c.InDegreeOf(v)
+		edges = make([]Edge, 0, deg)
+	} else {
+		// Otherwise just pick something...reasonable?
+		edges = make([]Edge, 0, 8)
+	}
+
+	g.EachArcTo(v, func(e Edge) (terminate bool) {
+		edges = append(edges, e)
+		return
+	})
+
+	return edges
+}
---
+++ util_test.go
@@ -0,0 +1,98 @@
+package gogl
+
+import (
+	. "github.com/sdboyer/gocheck"
+	"gopkg.in/fatih/set.v0"
+)
+
+// Tests for collection functors
+type CollectionFunctorsSuite struct {}
+var _ = Suite(&CollectionFunctorsSuite{})
+
+func (s *CollectionFunctorsSuite) TestCollectVertices(c *C) {
+	slice := CollectVertices(graphFixtures["3e5v1i"])
+
+	c.Assert(len(slice), Equals, 5)
+
+	set := set.NewNonTS()
+	for _, v := range slice {
+		set.Add(v)
+	}
+
+	c.Assert(set.Has("foo"), Equals, true)
+	c.Assert(set.Has("bar"), Equals, true)
+	c.Assert(set.Has("baz"), Equals, true)
+	c.Assert(set.Has("qux"), Equals, true)
+	c.Assert(set.Has("isolate"), Equals, true)
+}
+
+func (s *CollectionFunctorsSuite) TestCollectAdjacentVertices(c *C) {
+	slice := CollectVerticesAdjacentTo("foo", graphFixtures["3e5v1i"])
+
+	c.Assert(len(slice), Equals, 2)
+
+	set := set.NewNonTS()
+	for _, v := range slice {
+		set.Add(v)
+	}
+
+	c.Assert(set.Has("bar"), Equals, true)
+	c.Assert(set.Has("qux"), Equals, true)
+}
+
+func (s *CollectionFunctorsSuite) TestCollectEdges(c *C) {
+	slice := CollectEdges(graphFixtures["3e5v1i"])
+
+	c.Assert(len(slice), Equals, 3)
+
+	set := set.NewNonTS()
+	for _, e := range slice {
+		set.Add(e)
+	}
+
+	c.Assert(set.Has(BaseEdge{"foo", "bar"}), Equals, true)
+	c.Assert(set.Has(BaseEdge{"bar", "baz"}), Equals, true)
+	c.Assert(set.Has(BaseEdge{"foo", "qux"}), Equals, true)
+}
+
+func (s *CollectionFunctorsSuite) TestCollectEdgesIncidentTo(c *C) {
+	slice := CollectEdgesIncidentTo("foo", graphFixtures["3e5v1i"])
+
+	c.Assert(len(slice), Equals, 2)
+
+	set := set.NewNonTS()
+	for _, e := range slice {
+		set.Add(e)
+	}
+
+	c.Assert(set.Has(BaseEdge{"foo", "bar"}), Equals, true)
+	c.Assert(set.Has(BaseEdge{"foo", "qux"}), Equals, true)
+}
+
+func (s *CollectionFunctorsSuite) TestCollectArcsFrom(c *C) {
+	slice := CollectArcsFrom("foo", graphFixtures["arctest"].(DirectedGraph))
+
+	c.Assert(len(slice), Equals, 2)
+
+	set := set.NewNonTS()
+	for _, e := range slice {
+		set.Add(e)
+	}
+
+	c.Assert(set.Has(BaseEdge{"foo", "qux"}), Equals, true)
+	c.Assert(set.Has(BaseEdge{"foo", "bar"}), Equals, true)
+}
+
+func (s *CollectionFunctorsSuite) TestCollectArcsTo(c *C) {
+	slice := CollectArcsTo("bar", graphFixtures["arctest"].(DirectedGraph))
+
+	c.Assert(len(slice), Equals, 2)
+
+	set := set.NewNonTS()
+	for _, e := range slice {
+		set.Add(e)
+	}
+
+	c.Assert(set.Has(BaseEdge{"foo", "bar"}), Equals, true)
+	c.Assert(set.Has(BaseEdge{"qux", "bar"}), Equals, true)
+}
