648df3611fee24be84b0533eb7019c358a16caf6
Sam Boyer <tech@samboyer.org>
2014-08-08 09:55:19 -0400

    Shift counting suites into separate file.

--- spec/graph_spec.go
+++ spec/graph_spec.go
@@ -620,32 +620,3 @@ func (s *MutableDataGraphSuite) TestMultiAddAndRemoveEdge(c *C) {
 	c.Assert(g.HasEdge(NewEdge(2, 3)), Equals, false)
 }
 
-/* Counting suites - tests for Size() and Order() */
-
-type OrderSuite struct {
-	Factory  func(GraphSource) Graph
-	Directed bool
-}
-
-func (s *OrderSuite) SuiteLabel() string {
-	return fmt.Sprintf("%T", s.Factory(NullGraph))
-}
-
-func (s *OrderSuite) TestOrder(c *C) {
-	c.Assert(s.Factory(NullGraph).(VertexCounter).Order(), Equals, 0)
-	c.Assert(s.Factory(GraphFixtures["2e3v"]).(VertexCounter).Order(), Equals, 3)
-}
-
-type SizeSuite struct {
-	Factory  func(GraphSource) Graph
-	Directed bool
-}
-
-func (s *SizeSuite) SuiteLabel() string {
-	return fmt.Sprintf("%T", s.Factory(NullGraph))
-}
-
-func (s *SizeSuite) TestSize(c *C) {
-	c.Assert(s.Factory(NullGraph).(EdgeCounter).Size(), Equals, 0)
-	c.Assert(s.Factory(GraphFixtures["2e3v"]).(EdgeCounter).Size(), Equals, 2)
-}
